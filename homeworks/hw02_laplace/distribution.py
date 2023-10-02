fbc5720f6d5691ad68cc17823507ab5f494113ec
Ivan Panferov <panferov.ivan@huawei-partners.com>
2023-10-03 01:06:02 +0300

    Added Laplace homework
    Signed-off-by: Ivan Panferov <panferov.ivan@huawei-partners.com>

--- homeworks/hw02_laplace/distribution.py
+++ homeworks/hw02_laplace/distribution.py
@@ -11,6 +11,9 @@ class LaplaceDistribution:
         ####
         # Do not change the class outside of this block
         # Your code here
+        loc = np.median(x, axis=0)
+        scale = np.mean(np.abs(x - loc), axis=0)
+        return (loc, scale) 
         ####
 
     def __init__(self, features):
@@ -20,8 +23,8 @@ class LaplaceDistribution:
         '''
         ####
         # Do not change the class outside of this block
-        self.loc = # YOUR CODE HERE
-        self.scale = # YOUR CODE HERE
+
+        self.loc, self.scale = self.mean_abs_deviation_from_median(features)
         ####
 
 
@@ -33,7 +36,7 @@ class LaplaceDistribution:
         '''
         ####
         # Do not change the class outside of this block
-        return 
+        return (-np.abs(values - self.loc) / self.scale) - np.log(2 * self.scale)
         ####
         
     
@@ -43,4 +46,4 @@ class LaplaceDistribution:
         Args:
             values: A numpy array of shape (n_objects, n_features). Every column represents all available values for the selected feature.
         '''
-        return np.exp(self.logpdf(value))
+        return np.exp(self.logpdf(values))
